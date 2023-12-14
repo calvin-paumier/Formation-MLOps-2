4835f84cc7e2dd9feee2e03c7fff57ea50a125e4
calvin-paumier <calvin.paumier@hotmail.fr>
2023-12-14 15:40:25 +0000

    test mlflow

--- formation_indus_ds_avancee/train_and_predict.py
+++ formation_indus_ds_avancee/train_and_predict.py
@@ -6,6 +6,9 @@ import mlflow
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 
+mlflow.set_tracking_uri('https://0.0.0.0:36935')
+mlflow.set_experiment('test')
+
 
 def train_model_with_io(features_path: str, model_registry_folder: str) -> None:
     features = pd.read_parquet(features_path)
@@ -18,9 +21,14 @@ def train_model(features: pd.DataFrame, model_registry_folder: str) -> None:
     X = features.drop(columns=[target])
     y = features[target]
     with mlflow.start_run():
-        # insert autolog here ...
+        mlflow.sklearn.autolog(log_models=False)
         model = RandomForestRegressor(n_estimators=1, max_depth=10, n_jobs=1)
         model.fit(X, y)
+        mlflow.sklearn.log_model(
+            sk_model=model,
+            artifact_path="sklearn_model",
+            registered_model_name="sklearn_model"
+        )
     time_str = time.strftime('%Y%m%d-%H%M%S')
     joblib.dump(model, os.path.join(model_registry_folder, time_str + '.joblib'))
 
